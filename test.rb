0c5b0cc6ce4db30f1ac1e77f800183f0ec029f53
AlexeyPeov <111562626+AlexeyPeov@users.noreply.github.com>
2023-02-10 10:57:59 +0300

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,7 +1,14 @@
 puts "hello"
-def squareOf (a) 
-    return a*a;
+def factorial (a)
+    num = 1;
+
+    i = 1;
+    for i in 1..a
+        num *= i
+        i+=1
+    end
+
+    return num;
 end
-puts squareOf(5)
 
-
+puts factorial(5)
